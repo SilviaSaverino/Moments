f16731313193b3a032f8a0ef2c2df6ea23d00f61
SilviaSaverino <>
2023-05-18 12:12:07 +0000

    fix imports path for popular profiles component

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../api/axiosDefaults";
+import { axiosReq } from "../../api/axiosDefaults";
 import appStyles from "../App.module.css";
-import Asset from "../components/Asset";
-import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = ({mobile}) => {
   const [profileData, setProfileData] = useState({
